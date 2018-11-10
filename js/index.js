f335109d7299ad85b8f7d613c1eaf43715b1b725
Raxe88 <parareda8@gmail.com>
2018-11-10 12:41:44 +0100

    Afegides variables d'estats i animacions. Ara es pot obrir i tancar l'aplicacio vermella

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 //<![CDATA[
 var req = false;
 var randomSyncNumber = Math.floor((Math.random() * 8999) + 1000);
-var showSyncStatus = 0; // 0 -> No s'ha ensenyat, 1 -> s'està ensenyant, 2 -> està en un altre menú
+var showSyncStatus = 0; // 0 -> No s'ha ensenyat, 1 -> s'està ensenyant
+var showRedAppStatus = 0; //0 -> No s'ha ensenyat, 1 -> s'ha d'obrir, 2 -> està obert i s'ha de tancar
 
 window.onload = function() {
     registerKeyEventListener();
     initApp();
     setRandomSyncNumber();
-    showRed();
+    showRedInformativeMessage();
 };
 
 /**
@@ -17,37 +18,93 @@ window.onload = function() {
 function handleKeyCode(kc) {
     if (kc==VK_RED || kc == VK_ENTER) {
         showSync();
+        showRedApp();
         //document.location.href = './menu.html';
         return true;
     }
-    if(kc==VK_LEFT){
-        document.getElementById("firetv-background-tv").style.cssText = "";
-        document.getElementById("firetv-background-tv").childNodes[0].style.cssText = "";
-        document.getElementById("firetv-background-tv").childNodes[0].className = "ResizeTranslate";
+    if(kc==VK_0){
+        hideRedApp();
     }
     return false;
 };
 
+/**
+ * Funció que genera un nombre aleatori
+ */
 function setRandomSyncNumber(){
     document.getElementById("randomNumber").innerHTML = randomSyncNumber;
 }
 
+/**
+ * Funció que es crida quan prémen el boto vermell i ensenya el missatge de sincronització. També s'encarrega d'engegar l'aplicació quan prémen "ok".
+ */
 function showSync(){
+
+    //Si no han vist el missatge encara
     if(showSyncStatus == 0){
+        //Engegar l'animació d'ensenyar el missatge de sincronització
         document.getElementById("sync").className = "FadeIn";
+        //Amagar el missatge de prémer el botó vermell
         document.getElementById("pressRedButtonMessage").className = "FadeOut";
+        //Canviar l'estat del missatge de sincronització a ensenyant
         showSyncStatus++;
-    }else if(showSyncStatus == 1){
+
+    //Si s'està veient el missatge de sincronització i l'app no està engegada
+    }else if(showSyncStatus == 1 && showRedAppStatus < 1){
+        //Amagar el missatge de sincronització
         document.getElementById("sync").className = "FadeOut";
+
+        //Engengar l'app vermella
+        showRedAppStatus = 1;
     }
 };
+
+/**
+ * Funció que depenet de l'estat de la variable showRedAppStatus s'ensenya o s'amaga l'aplicació vermella
+ */
+async function showRedApp(){
+    
+    /*var newDiv = document.createElement('div');
+    newDiv.setAttribute("id", "newFireTV");
+    newDiv.setAttribute("style", "position: absolute; top: 0px !important; left: 0px !important");
+    var firetvnode = document.getElementById("firetv-background-tv");
+    document.childNodes[2].childNodes[2].remove(); //newDiv
+    document.childNodes[2].childNodes[2].appendChild(newDiv);
+    document.getElementById("newFireTV").appendChild(firetvnode);*/
+
+    //document.getElementById("firetv-background-tv").style.cssText = "";
+    //document.getElementById("firetv-background-tv").childNodes[0].className = "FadeOut";
+    //await sleep(2000);
+    //document.getElementById("firetv-background-tv").childNodes[0].style.cssText = "";
+    //document.getElementById("firetv-background-tv").childNodes[0].style.cssText = "position: absolute; top: 100px; left: 750px; width: 30%; height: 30%; min-width: 1px; min-height: 1px; transform-origin: 50% 50% 0px; visibility: inherit;";
+    switch(showRedAppStatus){
+        case 1: //Que s'hagi d'ensenyar
+            //Engegar animació
+            document.getElementById("firetv-background-tv").childNodes[0].className = "RedAppFadeIn";
+            await sleep(1000);
+            document.getElementById("firetv-background-tv").childNodes[0].className = "RedApp";
+            //S'està ensenyant l'animació
+            showRedAppStatus = 2;
+            break;
+        case 2: //Que s'hagi d'amagar l'aplicació perquè l'han tancada
+            document.getElementById("firetv-background-tv").childNodes[0].className = "";
+            
+            //Tornar tots els estats als originals
+            showRedAppStatus = 0;
+            showSyncStatus = 0;
+
+            //Ensenyar el missatge informatiu de prémer el botó vermell.
+            showRedInformativeMessage();
+            break;
+    }
+}
 
 /**
  * Funció asíncrona que permet que la resta del javascript s'executi sense interrompre.
  * Amb l'ús de la funció sleep i canviant les classes del component 'div' amb l'instrucció
  * de prémer el botó vermell es reprodueix l'animació de fadein i fadeout els cops que calen.
  */
-async function showRed(){
+async function showRedInformativeMessage(){
 
     //Si no s'ha obert encara l'aplicació
     if(showSyncStatus == 0){
