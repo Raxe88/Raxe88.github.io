93f4661aa6a23c012f68a150f85e1d03dd76cf73
Raxe88 <parareda8@gmail.com>
2018-11-11 20:58:32 +0100

    Falta fer les visites

--- js/index.js
+++ js/index.js
@@ -5,6 +5,9 @@ var showSyncStatus = 0; // 0 -> No s'ha ensenyat, 1 -> s'està ensenyant
 var showRedAppStatus = 0; //0 -> No s'ha ensenyat, 1 -> s'ha d'obrir, 2 -> està obert i s'ha de tancar
 var selectionBarPosition = 0;
 var selectedElement = 0;
+var showingVideo = false;
+var selectedVideo = -1;
+var isFullscreen = false;
 
 var gjson;
 
@@ -24,16 +27,22 @@ window.onload = function() {
  * @param {tecla} kc 
  */
 function handleKeyCode(kc) {
+    if(kc== VK_BLUE){
+        //document.getElementById("firetv-background-tv").childNodes[0].muted = true;
+        fullscreenVideo();
+    }
     if(kc== VK_GREEN){
-        cosa = document.getElementById("firetv-background-tv");
-        document.getElementById("firetv-background-tv").innerHTML = "";
+        //document.getElementById("firetv-background-tv").childNodes[0].muted = true;
+        pauseVideo();
     }
     if(kc== VK_YELLOW){
-        document.getElementById("firetv-background-tv").innerHTML = cosa;
+        //document.getElementById("firetv-background-tv").childNodes[0].muted = false;
+        stopVideo();
     }
     if (kc==VK_RED || kc == VK_ENTER) {
         showSync();
         showRedApp();
+        showVideo();
         return true;
     }
     if(kc==VK_UP && showRedAppStatus == 2){
@@ -123,6 +132,86 @@ function showSync(){
     }
 };
 
+async function showVideo(){
+    if(showRedAppStatus == 2 && (!showingVideo || selectedVideo != selectedElement)){
+        selectedVideo = selectedElement;
+        showingVideo = true;
+        document.getElementById("firetv-background-tv").childNodes[0].muted = true;
+        document.getElementById("firetv-background-tv").childNodes[0].className = "FadeOut";
+        await sleep(1000);
+        document.getElementById("newvideo").innerHTML = "<video class=\"videoSmall\" src=\"" + gjson.videoList[selectedElement].source + "\" autoplay=\"true\"></video>"    
+        document.getElementById("newvideo").childNodes[0].addEventListener('ended', onVideoFinish, false);
+        
+    }
+    if(showRedAppStatus == 2 && showingVideo && document.getElementById("newvideo").childNodes[0].paused){
+        document.getElementById("newvideo").childNodes[0].play();
+    }
+}
+
+function onVideoFinish(e) {
+    selectedVideo = -1;
+}
+
+async function stopVideo(){
+    if(showRedAppStatus == 2 && showingVideo){
+        if(isFullscreen){
+            isFullscreen = false;
+            document.getElementById("newvideo").childNodes[0].className = "FullscreenVideoOut";
+            await sleep(1000);
+            document.getElementById("newvideo").childNodes[0].className = "videoSmall";
+            document.getElementById("redButtonApp").className = "FadeIn";
+        }
+        showingVideo = false;
+        document.getElementById("newvideo").innerHTML = "";
+        document.getElementById("firetv-background-tv").childNodes[0].className = "RedApp FadeIn";
+        await sleep(1000);
+        document.getElementById("firetv-background-tv").childNodes[0].muted = false;  
+        //document.getElementById("firetv-background-tv").childNodes[0].className = "RedApp";
+    }
+}
+
+function pauseVideo(){
+    if(showRedAppStatus == 2 && showingVideo){
+        document.getElementById("newvideo").childNodes[0].pause();
+    }
+}
+
+async function fullscreenVideo(){
+    if(showRedAppStatus == 2){
+        if(showingVideo){
+            if(isFullscreen){
+                isFullscreen = false;
+                document.getElementById("pressBlueButtonMessage").className = "FadeOut";
+                document.getElementById("newvideo").childNodes[0].className = "FullscreenVideoOut";
+                await sleep(1000);
+                document.getElementById("newvideo").childNodes[0].className = "videoSmall";
+                document.getElementById("redButtonApp").className = "FadeIn";
+            }else{
+                showBlueInformativeMessage();
+                isFullscreen = true;
+                document.getElementById("newvideo").childNodes[0].className = "FullscreenVideoIn";
+                document.getElementById("redButtonApp").className = "FadeOut";
+                await sleep(1000);
+                document.getElementById("newvideo").childNodes[0].className = "FullscreenVideo";
+            }
+        }else{
+            if(isFullscreen){
+                isFullscreen = false;
+                document.getElementById("pressBlueButtonMessage").className = "FadeOut";
+                document.getElementById("firetv-background-tv").childNodes[0].className = "FullscreenVideoOut";
+                await sleep(1000);
+                document.getElementById("firetv-background-tv").childNodes[0].className = "RedApp";
+                document.getElementById("redButtonApp").className = "FadeIn";
+            }else{
+                showBlueInformativeMessage();
+                isFullscreen = true;
+                document.getElementById("redButtonApp").className = "FadeOut";
+                document.getElementById("firetv-background-tv").childNodes[0].className = "FullscreenVideoIn";
+            }
+        }
+    }
+}
+
 function initializeData(){
     for (var i = 0; i < gjson.userList.length; i++){
         document.getElementById("connectedUsers").childNodes[1].appendChild(document.createElement("li")).appendChild(document.createTextNode(gjson.userList[i]));
@@ -179,41 +268,28 @@ function scrollVideosDown(){
  * Funció que depenet de l'estat de la variable showRedAppStatus s'ensenya o s'amaga l'aplicació vermella
  */
 async function showRedApp(){
-    
-    /*var newDiv = document.createElement('div');
-    newDiv.setAttribute("id", "newFireTV");
-    newDiv.setAttribute("style", "position: absolute; top: 0px !important; left: 0px !important");
-    firetvnode = document.getElementById("firetv-background-tv");
-    document.childNodes[2].childNodes[2].remove(); //newDiv
-    document.childNodes[2].childNodes[2].appendChild(newDiv);
-    document.getElementById("newFireTV").appendChild(firetvnode);*/
+    if(showRedAppStatus == 1){
+        //Engegar animació
+        document.getElementById("firetv-background-tv").childNodes[0].className = "RedAppFadeIn";
+        document.getElementById("redButtonApp").className = "FadeIn redAppZPos";
+        await sleep(1000);
+        document.getElementById("firetv-background-tv").childNodes[0].className = "RedApp";
+        //S'està ensenyant l'animació
+        showRedAppStatus = 2;
+    }
+}
 
-    //document.getElementById("firetv-background-tv").style.cssText = "";
-    //document.getElementById("firetv-background-tv").childNodes[0].className = "FadeOut";
-    //await sleep(2000);
-    //document.getElementById("firetv-background-tv").childNodes[0].style.cssText = "";
-    //document.getElementById("firetv-background-tv").childNodes[0].style.cssText = "position: absolute; top: 100px; left: 750px; width: 30%; height: 30%; min-width: 1px; min-height: 1px; transform-origin: 50% 50% 0px; visibility: inherit;";
-    switch(showRedAppStatus){
-        case 1: //Que s'hagi d'ensenyar
-            //Engegar animació
-            document.getElementById("firetv-background-tv").childNodes[0].className = "RedAppFadeIn";
-            document.getElementById("redButtonApp").className = "FadeIn redAppZPos";
-            await sleep(1000);
-            document.getElementById("firetv-background-tv").childNodes[0].className = "RedApp";
-            //S'està ensenyant l'animació
-            showRedAppStatus = 2;
-            break;
-        case 2: //Que s'hagi d'amagar l'aplicació perquè l'han tancada
-            document.getElementById("firetv-background-tv").childNodes[0].className = "";
-            document.getElementById("redButtonApp").className = "FadeOut";
-            
-            //Tornar tots els estats als originals
-            showRedAppStatus = 0;
-            showSyncStatus = 0;
+async function hideRedApp(){
+    if(showRedAppStatus == 2){
+        document.getElementById("firetv-background-tv").childNodes[0].className = "";
+        document.getElementById("redButtonApp").className = "FadeOut";
+        
+        //Tornar tots els estats als originals
+        showRedAppStatus = 0;
+        showSyncStatus = 0;
 
-            //Ensenyar el missatge informatiu de prémer el botó vermell.
-            showRedInformativeMessage();
-            break;
+        //Ensenyar el missatge informatiu de prémer el botó vermell.
+        showRedInformativeMessage();
     }
 }
 
@@ -255,6 +331,24 @@ async function showRedInformativeMessage(){
         //S'amaga per sempre
         document.getElementById("pressRedButtonMessage").className = "FadeOut";
     }
+    
+};
+
+/**
+ * Funció asíncrona que permet que la resta del javascript s'executi sense interrompre.
+ * Amb l'ús de la funció sleep i canviant les classes del component 'div' amb l'instrucció
+ * de prémer el botó blue es reprodueix l'animació de fadein i fadeout els cops que calen.
+ */
+async function showBlueInformativeMessage(){
+
+    await sleep(1000);
+
+    //S'ensenya 10 segons
+    document.getElementById("pressBlueButtonMessage").className = "FadeIn";
+    await sleep(5000);
+
+    //S'amaga
+    document.getElementById("pressBlueButtonMessage").className = "FadeOut";
     
 };
 
